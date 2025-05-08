545665a68359eca8b26853036eea19eb29f12fe1
Misael Venoso <misael.venoso@gmail.com>
2025-05-08 00:11:11 -0600

    ADD: Se añaden imagenes y funcionamiento del sitio

---
+++ src/main/java/com/cenidet/Controller/testController.java
@@ -0,0 +1,36 @@
+package com.cenidet.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class testController {
+
+	@Autowired
+	
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/vuelos")
+	public String vuelos() {
+		return "vuelos";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	@GetMapping("/form_registros")
+	public String form_registros() {
+		return "form_registros";
+	}
+	
+	@GetMapping("/detalle_vuelo")
+	public String detalle_vuelo() {
+		return "detalle_vuelo";
+	}
+}
